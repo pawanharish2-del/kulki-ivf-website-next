aa5588b2b934b9975a42b27299c1b204b1d1299a
aryandadheech-programs <aryandadheech039@gmail.com>
2026-09-23 14:47:32 +0530

    tag manager

--- app/layout.tsx
+++ app/layout.tsx
@@ -37,11 +37,11 @@ export default function RootLayout({
         <script
           dangerouslySetInnerHTML={{
             __html: `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
-new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
-j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
-'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-})(window,document,'script','dataLayer','GTM-TXP7WRJV');`,
-          }}
+        new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+        j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+        'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+        })(window,document,'script','dataLayer','GTM-TXP7WRJV');`,
+                  }}
         />
         <link rel="icon" type="image/jpeg" href="/assets/images/feviconkulki.jpg" />
         <link
