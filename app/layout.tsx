5f606fb3688df9212192baacb380b26cb0c34e03
aryandadheech-programs <aryandadheech039@gmail.com>
2026-09-23 14:18:37 +0530

    add tag maenger

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,14 +3,10 @@ import "./globals.css";
 import LegacyInteractions from "@/components/layout/LegacyInteractions";
 
 export const metadata: Metadata = {
-  metadataBase: new URL("https://kulkiivfgroup.com"),
   title: "KULKI IVF Fertility & ART Centre | Jaipur",
   description:
     "Welcome to Kulki IVF Fertility & ART Center. Where Dreams of Parenthood Become Reality. Advanced fertility treatments in Jaipur.",
   keywords: "IVF Jaipur, KULKI IVF, fertility clinic Jaipur, ICSI IUI Jaipur, best IVF centre Jaipur",
-  verification: {
-    google: "PrU-NuxbFD9fh6eE-UNE3DrO4YJJeJW57iYwH0ZqFUE",
-  },
   openGraph: {
     title: "KULKI IVF Fertility & ART Centre | Jaipur",
     description: "Advanced IVF, ICSI, IUI and fertility treatments in Jaipur.",
@@ -37,6 +33,16 @@ export default function RootLayout({
   return (
     <html lang="en">
       <head>
+        {/* Google Tag Manager - Head Script */}
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+})(window,document,'script','dataLayer','GTM-TXP7WRJV');`,
+          }}
+        />
         <link rel="icon" type="image/jpeg" href="/assets/images/feviconkulki.jpg" />
         <link
           href="https://fonts.googleapis.com/css2?family=Poppins:wght@300;400;500;600;700&display=swap"
@@ -48,6 +54,16 @@ export default function RootLayout({
         />
       </head>
       <body>
+        {/* Google Tag Manager - Body (noscript) Script */}
+        <noscript>
+          <iframe
+            src="https://www.googletagmanager.com/ns.html?id=GTM-TXP7WRJV"
+            height="0"
+            width="0"
+            style={{ display: "none", visibility: "hidden" }}
+          />
+        </noscript>
+        
         <LegacyInteractions />
         {children}
       </body>
